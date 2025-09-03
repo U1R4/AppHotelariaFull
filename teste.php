a276985b5aeb244b7ec91e6aa8a13e69aa17bcb7
Uirá Cardoso <uira.guepardo@gmail.com>
2025-09-03 17:12:54 -0300

    mas coisa que foi pra teste
    bum

--- teste.php
+++ teste.php
@@ -9,8 +9,12 @@ $data = [
     'password' => '123'
 ];
 
-AuthController::login($conn,$data);
-echo validateToken($token);
+
+//AuthController::login($conn,$data);
+//$tokenNaovalido = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBIb3RlbGFyaWFGdWxsIiwiaWF0IjoxNzU2OTI3NjkzLCJleHAiOjE3NTY5MzEyOTMsInN1YiI6eyJpZCI6NCwiZW1haWwiOiJnZ2dAZ2dnLmdnZyIsIm5vbWUiOiJnZ2ciLCJjYXJnbyI6ImdnZyJ9fQ.RdIuqlK0QzITNN2oxMMrnnZdym4Dm6lS_u3O_rL8S3E";
+$token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBIb3RlbGFyaWFGdWxsIiwiaWF0IjoxNzU2OTMwMjMzLCJleHAiOjE3NTY5MzM4MzMsInN1YiI6eyJpZCI6NCwiZW1haWwiOiJnZ2dAZ2dnLmdnZyIsIm5vbWUiOiJnZ2ciLCJjYXJnbyI6ImdnZyJ9fQ.e56_nHM2O65MlEZCRE1FCRe0rXS9B9A86mVGGZYPVBc';
+echo var_dump(validateToken($token));
+
 //$hash = PassController::generateHash($data['password']);
 //echo $hash;
 //echo '<br>';
